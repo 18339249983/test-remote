0dd372e9e7c0d47e8a2b5566226ab70df7086d2a
userName <1099405909@qq.com>
2018-06-22 22:38:12 +0800

    url,views done

--- project/myApp/urls.py
+++ project/myApp/urls.py
@@ -1,5 +1,24 @@
 from django.conf.urls import url
 from myApp import views
 urlpatterns = [
+    url(r'^$', views.myweb),
+    url(r'^home/$', views.home),
+    url(r'^market/(\w+)/(\w+)/(\w+)/$', views.market),
+    url(r'^cart/$', views.cart),
+
+    url(r'^mine/$', views.mine),
+    # 登陆界面
+    url(r'^login/$', views.login),
+    url(r'^quit/$', views.quit),
+    url(r'^addAddress/$', views.addAddress),
+    url(r'^showAddress/$', views.showAddress),
+    url(r'detail/(\w+)/$', views.detail),
+    #     修改购物车  增加减少
+    url(r'^changecart/(\w+)/$', views.changecart),
+    # 修改购物车 是否选中
+    url(r'^changecart2/$', views.changecart2),
+    url(r'^qOrder/$', views.qOrder),
+    url(r'^map/$', views.map),
+    url(r'search/$', views.search),
 
 ]
--- project/myApp/views.py
+++ project/myApp/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from myApp.models import SlideShow, MainDescription, Product, CategorieGroup, ChildGroup, User, Address, Cart, Order
+import random
+# from dysms_python.demo_sms_send import send_sms
+from django.http import JsonResponse
+import uuid
 
 # Create your views here.
+def home(request):
+    pass
+def market(request, gid, cid, sid):
+    pass
+def mine(request):
+    pass
+def quit(request):
+    pass
+def login(request):
+    pass
+def map(request):
+    pass
+def showAddress(request):
+   pass
+def addAddress(request):
+    pass
+def cart(request):
+    pass
+def changecart(request, flag):
+    pass
+def changecart2(request):
+    pass
+def search(request):
+    pass
+def myweb(request):
+    pass
+
+
