a3d9b1388823492bf649ae64b8797ddfb0cddce0
userName <1099405909@qq.com>
2018-07-02 17:13:18 +0800

    first

--- project/myApp/views.py
+++ project/myApp/views.py
@@ -5,11 +5,23 @@ import random
 # from dysms_python.demo_sms_send import send_sms
 from django.http import JsonResponse
 import uuid
+from myApp.serializers import SlideShowSerializer,MainDescriptionSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
 
 # Create your views here.
+@csrf_exempt
 def home(request):
-    pass
+    if request.method == 'GET':
+        lbt = SlideShow.objects.all()
+        serializer = SlideShowSerializer(lbt,many=True)
+        return JsonResponse(serializer.data,safe=False)
 def market(request, gid, cid, sid):
+    pass
+def detail(request):
     pass
 def mine(request):
     pass
@@ -32,6 +44,8 @@ def changecart2(request):
 def search(request):
     pass
 def myweb(request):
+    pass
+def qOrder(request):
     pass
 
 
